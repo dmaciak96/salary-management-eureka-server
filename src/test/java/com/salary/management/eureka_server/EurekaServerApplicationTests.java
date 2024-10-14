40b81ec4bf9b129ec6f7cb2a9d1f8dd0b6f451b4
Dawid <dawid.maciak@huuugegames.com>
2024-10-14 12:29:35 +0200

    Init Eureka Server

---
+++ src/test/java/com/salary/management/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salary.management.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
